042960fb2716737c8eaabdcb331b39c2a8a19f29
ollxko <115688243+ollxko@users.noreply.github.com>
2025-10-19 14:23:42 +0500

    created project

--- README.md
+++
@@ -1,1 +0,0 @@
-# teachers_front
---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+      prettier, // Добавляем Prettier конфиг
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier: pluginPrettier, // Добавляем Prettier плагин
+    },
+    rules: {
+      // Дополнительные правила
+      'prettier/prettier': 'error', // Ошибки при нарушении Prettier правил
+      'react-hooks/exhaustive-deps': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+]);
