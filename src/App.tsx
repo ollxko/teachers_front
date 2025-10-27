13ce15ca8512ad9d0d310ff1a835bc744fe46c10
Sh1ney <vilensky.net@mail.com>
2025-10-27 18:58:53 +0500

    добавил страницу новости

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Header from './components/Header/Header';
 import News from './pages/news-page/news-page';
 import Courses from './pages/courses-page/courses-page';
 import Events from './pages/events-page/events-page';
+import NewsItem from './pages/news-item-page/news-item-page';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Routes>
         <Route path={'/news'} element={<News />} />
+        <Route path={'/news/1'} element={<NewsItem />} />
         <Route path={'/courses'} element={<Courses />} />
         <Route path={'/events'} element={<Events />} />
       </Routes>
