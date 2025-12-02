a27efb395821a262a63f0a2851a63674876b9d0d
Виленский Тимофей Иванович <tvilenskii@ussc.ru>
2025-12-02 20:00:53 +0500

    обновил страницу события

--- src/pages/event-item-page/event-item-page.tsx
+++ src/pages/event-item-page/event-item-page.tsx
@@ -1,15 +1,26 @@
 import type { JSX } from 'react';
 import './event-item-page.css'
 import { Link } from 'react-router-dom';
+import { Tag, Typography } from 'antd';
 import Linkify from 'react-linkify';
 import { useMemo } from 'react';
 import EnrollButton from '../../components/EnrollButton/EnrollButton';
+
+import {
+    CalendarOutlined,
+    FieldTimeOutlined,
+    EnvironmentOutlined
+} from '@ant-design/icons';
+
+const { Title, Text } = Typography;
 
 type EventItemProps = {
   title: string;
   content: string;
   date: string;
   time: string;
+  address: string;
+  image: string;
 };
 
 const EventData: EventItemProps = {
@@ -17,6 +28,8 @@ const EventData: EventItemProps = {
   content: 'Целевая аудитория: молодые специалисты\nСпикер: Мария Нечаева, старший методист ЕДУ\n\nПочему это будет полезно?\n\n+ Знакомство с моделями и этапами адаптации\n+ Осознание своего внутреннего состояния\n+ Анализ типичных проблем адаптации\n+ Овладение механизмами и инструментами облегчения адаптации',
   date: '19.9.2025',
   time: '14:00-15:00',
+  address: 'МАОУ СОШ 100',
+  image: '/5472179671505434589.jpg',
 };
 
 type DateFormatterProps = {
@@ -51,26 +64,43 @@ export default function EventItem(): JSX.Element {
         <div className='event-item-container'>
             <Link to="/events" className="back-link">← Назад к списку событий</Link>
 
-            <article className="event-item">
-                <h1>{EventData.title}</h1>
+            <div className='event-layout'>
+                <article className="event-item">
+                    <Title>{EventData.title}</Title>
 
-                <div className="event-item-datetime">
-                    <div className="datetime-item">
-                        <img src="/Date.svg" alt="Дата" className="datetime-icon" />
-                        Дата: <DateFormatter dateString={EventData.date} />
+                    <Tag color={"purple"}>{"Онлайн"}</Tag>
+                    <Tag color={"cyan"}>{"Вебинар"}</Tag>
+
+                    <div className="event-item-datetime">
+                        <div className="datetime-item">
+                            <CalendarOutlined />
+                            <Text type="secondary">Дата: <DateFormatter dateString={EventData.date} /></Text>
+                        </div>
+                        <div className="datetime-item">
+                            <FieldTimeOutlined />
+                            <Text type="secondary">Время: {EventData.time}</Text>
+                        </div>
+                        <div className="datetime-item">
+                            <EnvironmentOutlined />
+                            <Text type="secondary">Адрес: {EventData.address}</Text>
+                        </div>
                     </div>
-                    <div className="datetime-item">
-                        <img src="/Time.svg" alt="Дата" className="datetime-icon" />
-                        Время: {EventData.time}
+
+                    <div className="event-item-content">
+                        <Linkify><Text>{EventData.content}</Text></Linkify>
                     </div>
-                </div>
+                </article>
 
-                <div className="event-item-content">
-                    <Linkify>{EventData.content}</Linkify>
-                </div>
+                <div className='event-sidebar'>
+                    {EventData.image && (
+                        <div className='event-image-container'>
+                        <img src={EventData.image} alt={EventData.title} className='event-item-image' />
+                        </div>
+                    )}
 
-                <EnrollButton />
-            </article>
+                    <EnrollButton/>
+                </div>
+            </div>
         </div>
     );
 }
