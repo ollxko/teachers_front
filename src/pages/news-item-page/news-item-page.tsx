ffe4db8ad28f31736e66bff731e868280b289c88
Виленский Тимофей Иванович <tvilenskii@ussc.ru>
2025-12-02 21:22:18 +0500

    обновил страницу новости

--- src/pages/news-item-page/news-item-page.tsx
+++ src/pages/news-item-page/news-item-page.tsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import Linkify from 'react-linkify';
 import './news-item-page.css';
 import PopupImage from '../../components/PopupImage/PopupImage';
+import { Typography } from 'antd';
+
+const { Title, Text } = Typography;
 
 type NewsItemProps = {
   title: string;
@@ -14,24 +17,26 @@ type NewsItemProps = {
 const NewsData: NewsItemProps = {
   title: 'Об организации и проведении школьного этапа ВсОШ 25/26',
   content: 'Приказ №451-Д Министерства образования Свердловской области от 04.09.2025 "Об организации и проведении школьного этапа всероссийской олимпиады школьников в Свердловской области в 2025/2026 учебном году"\n\nЕще больше информации о ВСОШ размещается здесь https://domuchitela.profiedu.ru/?section_id=229',
-  date: '2025-09-10',
+  date: '15.11.2025',
   image: '/5472179671505434589.jpg'
 }
 
 export default function NewsItem(): JSX.Element {
-  return (
-    <div className="news-item-container">
-      <Link to="/news" className="back-link">← Назад к списку новостей</Link>
-      <article className="news-item">
-        <h1>{NewsData.title}</h1>
-        <time className="news-date">{NewsData.date}</time>
-        <div className="news-content">
-          <Linkify>{NewsData.content}</Linkify>
-        </div>
+	return (
+		<div className="page-item-container">
+      		<Link to="/news" className="back-link">← Назад к списку новостей</Link>
 
-        {NewsData.image && <PopupImage src={NewsData.image}/>}
-
-      </article>
-    </div>
-  );
+			<article className="news-item">
+				<Title>{NewsData.title}</Title>
+				<Text type="secondary" className='news-date'>{NewsData.date}</Text>
+				<div className='news-images'>
+					{NewsData.image && <PopupImage src={NewsData.image}/>}
+					{NewsData.image && <PopupImage src={NewsData.image}/>}
+				</div>
+				<div className="news-item-content">
+					<Linkify><Text>{NewsData.content}</Text></Linkify>
+				</div>
+			</article>
+    	</div>
+  	);
 }
