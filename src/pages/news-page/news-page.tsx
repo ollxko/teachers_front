686023badae653a0df5190b82c4caafc7a5c2766
Sh1ney <vilensky.net@mail.com>
2025-12-07 20:41:42 +0500

    обновил страницу новостей

--- src/pages/news-page/news-page.tsx
+++ src/pages/news-page/news-page.tsx
@@ -1,19 +1,25 @@
 import type { JSX } from 'react';
 import './news-page.css';
-import BigNewsCard from '../../components/NewsCards/BigNewsCard/BigNewsCard';
-import SmallNewsCard from '../../components/NewsCards/SmallNewsCard/SmallNewsCard';
+import NewsCard from '../../components/NewsCards/NewsCard/NewsCard';
 
 export default function News(): JSX.Element {
-  return (
-    <div className='news-сontainer'>
-      <div className='first-line-container'>
-        <BigNewsCard title={'Новость'} date={'10.10'} />
-        <div className='small-cards-container'>
-          <SmallNewsCard title={'Новость'} date={'10.10'} />
-          <SmallNewsCard title={'Новость'} date={'10.10'} />
-          <SmallNewsCard title={'Новость'} date={'10.10'} />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='news-сontainer'>
+			<div className='first-line-container'>
+				<div className='big-card-container'>
+					<NewsCard title={'Новость'} date={'10.10'} image='' size='big'/>
+				</div>
+				<div className='small-cards-container'>
+					<NewsCard title={'Новость'} date={'10.10'} image='' size='small'/>
+					<NewsCard title={'Новость'} date={'10.10'} image='' size='small'/>
+					<NewsCard title={'Новость'} date={'10.10'} image='' size='small'/>
+				</div>
+			</div>
+			<div className='next-line-container'>
+				<NewsCard title={'Новость'} date={'10.10'} image='' size='medium'/>
+				<NewsCard title={'Новость'} date={'10.10'} image='' size='medium'/>
+				<NewsCard title={'Новость'} date={'10.10'} image='' size='medium'/>
+			</div>
+		</div>
+	);
 }
